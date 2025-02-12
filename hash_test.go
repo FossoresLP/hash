ddbbb12f750c428a4309c4803a74f72d6291fb61
Pascal Vorwerk <info@fossores.de>
2025-02-12 02:52:28 +0100

    test: string and []byte benchmarks
    Add specific benchmarks to test characteristics of `string` and `[]byte` handling separately.
    Also adds a fully typed map in addition to `map[string]any`.

--- hash_test.go
+++ hash_test.go
@@ -1,6 +1,7 @@
 package hash_test
 
 import (
+	"crypto/rand"
 	"crypto/sha256"
 	"testing"
 	"time"
@@ -12,6 +13,7 @@ type Example struct {
 	Name   string
 	Values []int
 	Detail map[string]interface{}
+	Typed  map[string]string
 	Nested struct {
 		SubName   string
 		Recursive *Example
@@ -47,6 +49,9 @@ func BenchmarkHashStruct(b *testing.B) {
 			"info":   "example struct for hashing",
 			"floats": []float64{1.2, 3.4, 5.6, 7.8, 9.0},
 		},
+		Typed: map[string]string{
+			"key": "value",
+		},
 		Nested: struct {
 			SubName   string
 			Recursive *Example
@@ -56,6 +61,7 @@ func BenchmarkHashStruct(b *testing.B) {
 				Name:   "recursive",
 				Values: []int{4, 5, 6},
 				Detail: nil,
+				Typed:  nil,
 				Nested: struct {
 					SubName   string
 					Recursive *Example
@@ -132,5 +138,50 @@ func BenchmarkHashStructRealistic(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			_ = hash.HashWithHash(s, sha256.New())
 		}
+	})
+}
+
+func BenchmarkHashString(b *testing.B) {
+	s := "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id consectetur purus ut faucibus pulvinar. Ornare quam viverra orci sagittis eu volutpat odio facilisis. Convallis a cras semper auctor. Nec dui nunc mattis enim ut tellus elementum. Ut tellus elementum sagittis vitae et leo duis. Luctus accumsan tortor posuere ac ut consequat semper viverra. Nibh cras pulvinar mattis nunc sed blandit libero volutpat sed. Viverra justo nec ultrices dui sapien eget. Eros donec ac odio tempor. Dapibus ultrices in iaculis nunc sed. Suscipit adipiscing bibendum est ultricies. Vulputate odio ut enim blandit volutpat maecenas volutpat. Vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet. Neque vitae tempus quam pellentesque nec. Hendrerit dolor magna eget est lorem ipsum dolor sit. Id diam maecenas ultricies mi eget mauris pharetra. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus.\n\nProin libero nunc consequat interdum varius sit amet mattis vulputate. Viverra ipsum nunc aliquet bibendum. Tempor orci dapibus ultrices in. In iaculis nunc sed augue. Nam libero justo laoreet sit amet cursus sit amet dictum. Dui nunc mattis enim ut tellus. Sed arcu non odio euismod lacinia at quis. Dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Gravida rutrum quisque non tellus orci ac. Velit dignissim sodales ut eu sem integer vitae justo eget. Auctor eu augue ut lectus arcu bibendum at. Turpis massa tincidunt dui ut ornare lectus sit amet. Dui sapien eget mi proin sed libero. Proin libero nunc consequat interdum varius. Lectus mauris ultrices eros in cursus turpis. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac. Aliquet enim tortor at auctor urna nunc."
+	b.Run("xxhash64", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = hash.Hash(s)
+		}
+	})
+	b.Run("xxhash128", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = hash.Hash128(s)
+		}
+	})
+	b.Run("sha256", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = hash.HashWithHash(s, sha256.New())
+		}
+	})
+}
+
+func BenchmarkHashBytes(b *testing.B) {
+	bts := make([]byte, 16384)
+	_, err := rand.Read(bts)
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.Run("xxhash64", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = hash.Hash(bts)
+		}
+		b.SetBytes(int64(len(bts)))
+	})
+	b.Run("xxhash128", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = hash.Hash128(bts)
+		}
+		b.SetBytes(int64(len(bts)))
+	})
+	b.Run("sha256", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = hash.HashWithHash(bts, sha256.New())
+		}
+		b.SetBytes(int64(len(bts)))
 	})
 }
